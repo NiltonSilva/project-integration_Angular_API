dab929e57a4db85664df81f5a1ff3c259ab479ed
Nilton S Ferreira <nsfgeo@hotmail.com>
2023-01-13 14:46:40 -0300

    Implementado integração com API

--- src/app/service/livro.service.ts
+++ src/app/service/livro.service.ts
@@ -1,7 +1,7 @@
-import { LivrosResultado } from './../models/interfaces';
+import { Item, LivrosResultado } from './../models/interfaces';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,11 +14,15 @@ export class LivroService {
 
   // Observer representa a ideia de uma coleção de callbacks, que "ouvi" os valores entregues pelos Observables; ele é usado por meio do subscribe() fica na minha classe .ts
   // Observable representa uma ideia de uma coleção de valores ou coleção de eventos futuros
-  buscar(valorDigitado: string): Observable<LivrosResultado> {
+  buscar(valorDigitado: string): Observable<Item[]> {
     const params = new HttpParams().append('q', valorDigitado);
     return this.http.get<LivrosResultado>(this.API, { params })
      // .()pipe serve para agrupar diversos outros tipos de operadores; é por onde vai passar o fluxo de informações.
-    .pipe(tap((retornoAPI) => console.log('Fluxo do TAP', retornoAPI))
+    .pipe(
+      tap((retornoAPI) => console.log('Fluxo do TAP', retornoAPI)),
+      map(resultado => resultado.items),
+      tap(resultado => console.log('Fluxo após o MAP', resultado)
+      )
     )
   }
 
